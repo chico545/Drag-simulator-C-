323cfbd6f3bbb926202da69b83d244722b36a8ae
chico545 <88053974+chico545@users.noreply.github.com>
2021-12-05 00:46:40 -0500

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -2768,12 +2768,19 @@ var veracruz = new Queen("Veracruz", 9, 9, 9, 10, 12, 12, 7, "Veracruz");
 var lmd = [aurora, cpher, elektravandergeld, georgiana, irisxc, lacarrera, leexafox, lupita, morralisa, papercut, rebelmork, sirena, tiresias, veracruz];
 //LA MAS DRAGA SEASON 2
 var abhora = new Queen("Abhora", 7, 7, 8, 11, 9, 10, 8, "Abhora");
+var astrud = new Queen("Astrud Aurelia", 5, 5, 8, 12, 9, 9, 6, "Astrud");
+var biqtch = new Queen("Biqtch Puddin", 9, 10, 5, 4, 9, 4, 8, "Biqtch");
 var dahli = new Queen("Dahli", 9, 9, 9, 9, 9, 9, 9, "Dahli");
 var dollyablack = new Queen("Dollya Black", 6, 7, 9, 11, 10, 11, 10, "Dollyablack");
+var evah = new Queen("Evah Destruction", 7, 7, 8, 9, 9, 6, 6, "EvahD");
+var hoso = new Queen("Hoso Terra Toma", 7, 8, 4, 12, 5, 13, 5, "Hoso");
 var jamesm = new Queen("James Majesty", 7, 7, 8, 11, 8, 11, 10, "Jamesm");
 var landoncider = new Queen("Landon Cider", 8, 8, 8, 11, 11, 9, 9, "Landoncider");
 var priscillachambers = new Queen("Priscilla Chambers", 9, 9, 8, 6, 8, 6, 9, "priscillachambers");
-var lmd_2 = [abhora, dahli, dollyablack, jamesm, landoncider, priscillachambers];
+var saint = new Queen("Saint", 7, 6, 9, 9, 6, 12, 3, "Saint");
+var sigourney = new Queen("Sigourney Beaver", 10, 8, 10, 9, 8, 10, 9, "Sigourney");
+var victoriaelizabeth = new Queen("Victoria Elizabeth Black", 8, 6, 9, 14, 8, 15, 9, "Victoriaelizabeth");
+var lmd_2 = [abhora, astrud, biqtch, dahli, dollyablack, evah, hoso, jamesm, landoncider, priscillachambers, saint, sigourney, victoriaelizabeth];
 //SPECIAL
 var pangina = new Queen("Pangina Heals", 10, 10, 9, 12, 9, 13, 11);
 var international_as = [baga, blu, cheryl, janey, jimbo, jujubee, lemon, monique, pangina];
@@ -2803,6 +2810,7 @@ var allQueens = [
     anita, art, cocoj, elektra, etc, jojo, karen, kita, maxi,
     arantxa, carmenf, dovima, drag, hugaceo, inti, killer, pupi, sagittaria, macarena,
     aurora, cpher, elektravandergeld, georgiana, irisxc, lacarrera, leexafox, lupita, morralisa, papercut, rebelmork, sirena, tiresias, veracruz,
+    abhora, astrud, biqtch, dahli, dollyablack, evah, hoso, jamesm, landoncider, priscillachambers, saint, sigourney, victoriaelizabeth,
     abhora, dahli, dollyablack, jamesm, landoncider, priscillachambers,
     pangina, 
 ].sort(function (a, b) { return a.getName().toLowerCase().localeCompare(b.getName().toLowerCase()); });
